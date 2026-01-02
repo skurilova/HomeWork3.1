ba9508b5fb1876fdb8d2d1d830ed85a148ff0d53
Svitlana Kurilova <kourilova.claire@gmail.com>
2026-01-02 15:41:57 +0100

    Change after connecting to GitHub

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("First Change");
 Console.WriteLine("Second Change");
+Console.WriteLine("Change after connecting to GitHub");
